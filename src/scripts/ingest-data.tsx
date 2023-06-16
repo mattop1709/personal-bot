e1a81bac3e5f0b7f0cb14ec811f0cb4795ec3709
Mohammad Burhan <mohammadburhan@Mohammads-Mac-mini.local>
2023-06-16 08:19:20 +0800

    load chunks to vector db

---
+++ src/scripts/ingest-data.tsx
@@ -0,0 +1,52 @@
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {OpenAIEmbeddings} from "langchain/embeddings/openai";
+import {PineconeStore} from "langchain/vectorstores/pinecone";
+import {pinecone} from "src/utils/Pinecone";
+import {PDFLoader} from "langchain/document_loaders/fs/pdf";
+import {PINECONE_INDEX_NAME, PINECONE_NAME_SPACE} from "src/configs/Pinecone";
+import {DirectoryLoader} from "langchain/document_loaders/fs/directory";
+
+const filePath = "docs";
+
+export async function run() {
+  try {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath, {
+      ".pdf": (path) => new PDFLoader(path),
+    });
+
+    // const loader = new PDFLoader(filePath);
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log("split docs", docs);
+
+    // console.log("creating vector store...");
+
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    // console.log(embeddings);
+    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE,
+      textKey: "text",
+    });
+  } catch (error) {
+    console.log("error", error);
+    throw new Error("Failed to ingest your data");
+  }
+}
+
+(async () => {
+  await run();
+  console.log("run script");
+})();
